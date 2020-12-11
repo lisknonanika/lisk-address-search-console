744f3008a037babdeb3cb4a33416d3ef6ce7b9e7
ysmdmg <44485074+lisknonanika@users.noreply.github.com>
2020-12-11 10:06:09 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+const { Mnemonic } = require('@liskhq/lisk-passphrase');
+const cryptography = require('@liskhq/lisk-cryptography');
+
+const outputHelp = () => {
+  console.log("[command]");
+  console.log("lisk-address-search-console help");
+  console.log("");
+  console.log("");
+  console.log("lisk-address-search-console [type] [word] [continue]");
+  console.log("  type: required");
+  console.log("    0 - forward match");
+  console.log("    1 - backward match");
+  console.log("    2 - partial match");
+  console.log("");
+  console.log("  word: required");
+  console.log("    Enter your favorite word");
+  console.log("    *half-width alphanumeric");
+  console.log("");
+  console.log("  continue: optional");
+  console.log("    0 - Stop after found (default)");
+  console.log("    1 - Continue even if found");
+  console.log("");
+  console.log("  e.g. lisk-address-search-console 0 cat 1");
+  console.log("");
+}
+
+const search = (_type, _word, _isContinue) => {
+  if (["0", "1", "2"].indexOf(_type) < 0) {
+    console.error("parameter error: [type].");
+    console.error("*should be 0 or 1 or 2.");
+    return;
+  }
+
+  if (!_word.match(/^[A-Za-z0-9]*$/)) {
+    console.error("parameter error: [_word].");
+    console.error("*should be half-width alphanumeric.");
+    return;
+  }
+
+  while(true) {
+    const passphrase = Mnemonic.generateMnemonic();
+    const { address, publicKey } = cryptography.getAddressAndPublicKeyFromPassphrase(passphrase);
+    const addressBip32 = cryptography.getBase32AddressFromAddress(address);
+  
+    if ((_type==="0" && addressBip32.startsWith(`lsk${_word.toLowerCase()}`)) ||
+        (_type==="1" && addressBip32.endsWith(_word.toLowerCase())) ||
+        (_type==="2" && addressBip32.indexOf(_word.toLowerCase()) > 0)) {
+      
+      console.info({
+        account: {
+          address: addressBip32,
+          baddress: cryptography.bufferToHex(address),
+          publickey: cryptography.bufferToHex(publicKey),
+          passphrase: passphrase,
+        }
+      });
+      if (!_isContinue) break;
+    }
+  }
+}
+
+(() => {
+  const args = process.argv;
+  if (args.length === 2 || args[2] === "help") {
+    outputHelp();
+  }
+  else if (args.length === 4) {
+    search(args[2], args[3], 0);
+  }
+  else if (args.length > 4) {
+    search(args[2], args[3], args[4]);
+  }
+  return;
+})();
